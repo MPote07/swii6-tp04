bfc9c36790b5e2d92982dcce82ab5b17c0765e18
Miguel da Silva Pataro <pataro.miguel@gmail.com>
2024-12-17 22:40:38 -0300

    comit

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace TP04.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public string? ISBN { get; set; }
+        public int Year { get; set; }
+    }
+}
+
+// Miguel Pataro CB3021564 e Lucas Vieira CB3020223
---
+++ Models/LibraryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TP04.Models
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
+
+// Miguel Pataro CB3021564 e Lucas Vieira CB3020223
